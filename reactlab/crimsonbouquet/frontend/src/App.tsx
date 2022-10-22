3adcd9fe465fcd1392c9c28c19ea8d0bb4b3685d
Varun Suraj <varunsuraj@college.harvard.edu>
2022-10-22 16:13:15 -0400

    react assignment 10/22

--- reactlab/crimsonbouquet/frontend/src/App.tsx
+++ reactlab/crimsonbouquet/frontend/src/App.tsx
@@ -4,18 +4,94 @@ import logo from './logo.svg';
 import './App.css';
 import { gql, useQuery } from '@apollo/client'
 
+const GET_ALL_CONTENT = gql`
+  query GetAllContent {
+    allContent {
+      title
+      slug
+    }
+  }
+`
+
+const GET_ARTICLE = gql`
+  query GetArticle($slug: String!) {
+    content(slug: $slug) {
+      title
+      text
+      contributors {
+        firstName
+        lastName
+      }
+    }
+  }
+`
+
+const GET_ALL_WRITERS = gql`
+  query GetAllWriters {
+    allContributors {
+      id
+      firstName
+      lastName
+    }
+  }
+`
+
+interface ArticleGQL {
+  title: string;
+  slug: string;
+}
+
+interface ContributorGQL {
+  id: number
+  firstName: string;
+  lastName: string;
+}
+
 const MainPage = function() {
+  const { loading: contentLoading, error: contentError, data: contentData } = useQuery(GET_ALL_CONTENT);
+  const { loading: writerLoading, error: writerError, data: writerData } = useQuery(GET_ALL_WRITERS);
+
+  if (contentLoading || writerLoading) return <p>Loading ...</p>;
+  if (contentError || writerError) return <p>Error :(</p>;
+
+
+
+  const articleLists = contentData.allContent.map((a: ArticleGQL) => (
+    <li key={a.slug}><Link to={'article/' + a.slug}>{a.title}</Link></li>
+  ));
+
+  const writerLists = writerData.allContributors.map((a: any) => (
+    <li key={a.id}>{a.firstName} {a.lastName}</li>
+  ));
+
   return (
     <div className="MainPage">
-      i am the main page now
+      <h1>Articles</h1>
+      <ul>
+        { articleLists }
+      </ul>
+      <h1>Writers</h1>
+      <ul>
+        { writerLists }
+      </ul>
     </div>
   )
 }
 
 const ArticlePage = function() {
+  const params = useParams()
+  console.log(params);
+  const {loading, error, data} = useQuery(GET_ARTICLE, {variables: params})
+  
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log(data)
+
   return (
     <div className="article">
-      i am a page lol
+      <h1> {data.content.title} </h1>
+      <div className="body" dangerouslySetInnerHTML={{__html: data.content.text}} />
     </div>
   )
 }
